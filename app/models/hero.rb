72c670f0d13f96bae585e882e9f98f89778a37f7
carvalhocaio <caio.carvalho@trustedhealth.com>
2022-02-07 21:10:28 -0400

    feat: :sparkles: add token column in heroes

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,6 +1,7 @@
 class Hero < ApplicationRecord
-  validates :name, presence: true
+  validates :name, :token, presence: true
 
+  scope :search, ->(term) { where('LOWER(name) LIKE ?', "%#{term.downcase}%") }
   scope :sorted_by_name, -> { order(:name) }
   # scope :sorted_by_name, -> { order(name: :desc) }
 end
